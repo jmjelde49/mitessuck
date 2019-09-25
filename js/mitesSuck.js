064a878070dfd6e2374be707351436e6a1018138
Jessica Mjelde <jessica_mjelde@hotmail.com>
2019-09-25 11:22:30 -0400

    Added test object and array

--- js/mitesSuck.js
+++ js/mitesSuck.js
@@ -1,1 +1,29 @@
+var treatmentsArray=[['1 Name', '1 Description', '1 Instructions', '1 Video', '1 FAQ', '1 More'],
+				['2 Name', '2 Description', '2 Instructions', '2 Video', '2 FAQ', '2 More'],
+				['3 Name', '3 Description', '3 Instructions', '3 Video', '3 FAQ', '3 More']];
 
+var treatmentsObject={
+	'treatmentOne': {"name": "1 name",
+			"description": "1 description",
+			"instructions": "1 instructions",
+			"video": "1 video",
+			"faq": "1 FAQ",
+			"more": "1 more"
+	},
+	'treatmentTwo': {"name": "2 name",
+			"description": "2 description",
+			"instructions": "2 instructions",
+			"video": "2 video",
+			"faq": "2 FAQ",
+			"more": "2 more"
+	},
+	'treatmentThree': {"name": "3 name",
+			"description": "3 description",
+			"instructions": "3 instructions",
+			"video": "3 video",
+			"faq": "3 FAQ",
+			"more": "3 more"
+	}
+};
+console.log(treatmentsArray[0]);
+console.log(treatmentsObject.treatmentThree)
