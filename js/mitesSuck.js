82cb977bf7ff517241cb40f6652b7f4df37452ae
Jessica Mjelde <jessica_mjelde@hotmail.com>
2019-09-25 13:40:20 -0400

    Improved treatment object and array

--- js/mitesSuck.js
+++ js/mitesSuck.js
@@ -1,6 +1,6 @@
-var treatmentsArray=[['1 Name', '1 Description', '1 Instructions', '1 Video', '1 FAQ', '1 More'],
-				['2 Name', '2 Description', '2 Instructions', '2 Video', '2 FAQ', '2 More'],
-				['3 Name', '3 Description', '3 Instructions', '3 Video', '3 FAQ', '3 More']];
+var treatmentsArray=[['1 Name', '1 Description', '1 Instructions', '1 Video', '1 FAQ', '1 More', true, false, false],
+				['2 Name', '2 Description', '2 Instructions', '2 Video', '2 FAQ', '2 More', true, false, true],
+				['3 Name', '3 Description', '3 Instructions', '3 Video', '3 FAQ', '3 More', false, false, true]];
 
 var treatmentsObject={
 	'treatmentOne': {"name": "1 name",
@@ -8,21 +8,30 @@ var treatmentsObject={
 			"instructions": "1 instructions",
 			"video": "1 video",
 			"faq": "1 FAQ",
-			"more": "1 more"
+			"more": "1 more",
+			"organic": false,
+			"brood": true,
+			"supers": false
 	},
 	'treatmentTwo': {"name": "2 name",
 			"description": "2 description",
 			"instructions": "2 instructions",
 			"video": "2 video",
 			"faq": "2 FAQ",
-			"more": "2 more"
+			"more": "2 more",
+			"organic": false,
+			"brood": true,
+			"supers": false
 	},
 	'treatmentThree': {"name": "3 name",
 			"description": "3 description",
 			"instructions": "3 instructions",
 			"video": "3 video",
 			"faq": "3 FAQ",
-			"more": "3 more"
+			"more": "3 more",
+			"organic": true,
+			"brood": true,
+			"supers": false
 	}
 };
 console.log(treatmentsArray[0]);
